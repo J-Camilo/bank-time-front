c78b4e470506ee93f7dfd89ec9a2979a18c50667
J-Camilo <jc4milo311@gmail.com>
2026-05-03 08:23:13 -0500

    se quita el campo de recuperar contrasena y se coloca la api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -67,7 +67,7 @@ export default function Login() {
               {loading ? 'Cargando...' : 'Iniciar sesión'}
             </button>
             <div className="text-center space-y-2">
-              <p className="text-sm text-sky-mid cursor-pointer hover:underline">¿Olvidaste tu contraseña?</p>
+              {/* <p className="text-sm text-sky-mid cursor-pointer hover:underline">¿Olvidaste tu contraseña?</p> */}
               <p className="text-sm text-gray-600">
                 ¿Aún no tienes cuenta?{' '}
                 <Link to="/register" className="text-navy font-semibold hover:underline">Regístrate aquí</Link>
